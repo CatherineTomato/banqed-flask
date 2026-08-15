e85f640642cf1f6e6a56eb9ffb5b4fc8bff5a2a5
CatherineTomato <grehanc1@gmail.com>
2026-08-15 22:42:43 +0100

    Connect Flask app to SQLite database

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banqed.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
+    with app.app_context():
     app.run(debug=True)
